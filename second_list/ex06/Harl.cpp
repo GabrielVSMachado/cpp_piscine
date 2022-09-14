be42358a1907f3717075a62f7f5c445b92f298b5
Gabriel Vitor Sales Machado <gabriel.machado@loft.com.br>
2022-09-14 19:00:09 -0300

    finish exercise 06 from second list

--- second_list/ex06/Harl.cpp
+++ second_list/ex06/Harl.cpp
@@ -12,36 +12,40 @@
 
 #include "Harl.hpp"
 
+Harl::HarlMemFn Harl::f[4] = {
+  &Harl::_debug, &Harl::_error, &Harl::_info, &Harl::_warning
+};
+
 Harl::Harl(void) { return; }
 Harl::~Harl(void) { return; }
 
 void Harl::_debug(void) const {
   std::cout << "I love having extra bacon for my ";
-  std::cout << "7XL-double-cheese-triple-pickle-special-ketchup burger. ";
+  std::cout << "7XL-double-cheese-triple-pickle-special-ketchup burger.";
+  std::cout << std::endl;
   std::cout << "I really do!" << std::endl;
 }
 
 void Harl::_info(void) const {
-  std::cout << "I cannot believe adding extra bacon cost more money. ";
-  std::cout << "You didn't put enough bacon in my burguer! ";
+  std::cout << "I cannot believe adding extra bacon cost more money.";
+  std::cout << std::endl;
+  std::cout << "You didn't put enough bacon in my burguer!" << std::endl;
   std::cout << "If you did, I wouldn't be asking for more!" << std::endl;
 }
 
 void Harl::_warning(void) const {
-  std::cout << "I think I deserve to have some extra bacon for free. ";
+  std::cout << "I think I deserve to have some extra bacon for free.";
+  std::cout << std::endl;
   std::cout << "I've been coming for years whereas ";
   std::cout << "you starting working here since last month." << std::endl;
 }
 
 void Harl::_error(void) const {
-  std::cout << "This is unacceptable! I want to speak with your manager now.";
+  std::cout << "This is unacceptable, I want to speak with your manager now.";
   std::cout << std::endl;
 }
 
 void Harl::complain(std::string level) const {
-  HarlMemFn const f[4] = {
-    &Harl::_debug, &Harl::_error, &Harl::_info, &Harl::_warning
-  };
   unsigned int nlevel = (level[0] % 16) - 4;
-  (this->*(f[ nlevel < 4 ? nlevel : nlevel - 3 ]))();
+  (this->*(Harl::f[ nlevel < 4 ? nlevel : nlevel - 3 ]))();
 }
--- second_list/ex06/main.cpp
+++ second_list/ex06/main.cpp
@@ -3,8 +3,7 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gvitor-s <gvitor-s@student.42sp.org.br>    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
+/*   By: gvitor-s <gvitor-s@student.42sp.org.br>    +#+  +:+       +#+        */ /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/14 00:03:25 by gvitor-s          #+#    #+#             */
 /*   Updated: 2022/09/14 00:05:11 by gvitor-s         ###   ########.fr       */
 /*                                                                            */
@@ -33,10 +32,32 @@ static unsigned int calcWordHash(char *word) {
 int main(int argc, char *argv[]) {
 
   Harl harl;
-  std::size_t value;
+
+  if (argc != 2) {
+    return 1;
+  }
 
   switch (calcWordHash(argv[1])) {
+    case DEBUG:
+      std::cout << "[ DEBUG ]" << std::endl;
+      harl.complain("DEBUG");
+      std::cout << std::endl;
+    case INFO:
+      std::cout << "[ INFO ]" << std::endl;
+      harl.complain("INFO");
+      std::cout << std::endl;
+    case WARNING:
+      std::cout << "[ WARNING ]" << std::endl;
+      harl.complain("WARNING");
+      std::cout << std::endl;
+    case ERROR:
+      std::cout << "[ ERROR ]" << std::endl;
+      harl.complain("ERROR");
+      std::cout << std::endl;
+      break;
     default:
+      std::cout << "[ Probably complaining about insignificant problems ]";
+      std::cout << std::endl;
       break;
   }
 
