36e8d768f00ff09d2aade5c821ae233055e35f63
gvitor-s <gvitor-s@student.42sp.org.br>
2022-10-12 18:43:58 -0300

    fix: megaphone

--- first_list/ex00/megaphone.cpp
+++ first_list/ex00/megaphone.cpp
@@ -10,24 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <cctype>
 #include <iostream>
-#include <locale>
-
-static std::string trimSpaces(std::string word) {
-  std::size_t firstSpace;
-  std::size_t lastSpace;
-
-  if (word[0] != ' ' && word[word.length() - 1] != ' ')
-    return word;
-  firstSpace = -1;
-  while (word[++firstSpace] == ' ')
-    ;
-  lastSpace = word.length();
-  while (word[lastSpace - 1] == ' ')
-    --lastSpace;
-  return word.substr(firstSpace, lastSpace - firstSpace);
-}
 
 int main(int argc, char *argv[]) {
   std::string word;
@@ -39,12 +22,9 @@ int main(int argc, char *argv[]) {
 
   for (int i = 1; i < argc; i++) {
     word = argv[i];
-    word = trimSpaces(word);
-    for (int j = 0; j < word.length(); j++) {
+    for (size_t j = 0; j < word.length(); j++) {
       std::cout << static_cast<char>(std::toupper(word[j]));
     }
-    if (argv[i + 1])
-      std::cout << ' ';
   }
   std::cout << std::endl;
   return 0;
