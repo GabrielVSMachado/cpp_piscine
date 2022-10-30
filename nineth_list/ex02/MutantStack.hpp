ed8c414fe245383e05050843c10b4671bf68eb8b
gvitor-s <gvitor-s@student.42sp.org.br>
2022-10-30 17:14:55 -0300

    feat: finish all cpp piscine

--- nineth_list/ex02/MutantStack.hpp
+++ nineth_list/ex02/MutantStack.hpp
@@ -6,29 +6,28 @@
 /*   By: gvitor-s <gvitor-s@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/27 23:20:36 by gvitor-s          #+#    #+#             */
-/*   Updated: 2022/10/27 23:47:08 by gvitor-s         ###   ########.fr       */
+/*   Updated: 2022/10/29 00:45:49 by gvitor-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MUTANTSTACK_HPP
 #define MUTANTSTACK_HPP
 
+#include <deque>
 #include <stack>
-#include <iterator>
 
 template<typename T>
 class MutantStack : public std::stack<T> {
-public:
-  MutantStack();
-  MutantStack(MutantStack const &);
-  MutantStack &operator=(MutantStack const &);
-  virtual ~MutantStack();
+  public:
+    MutantStack() {}
+    MutantStack(MutantStack const &other) { *this = other; }
+    MutantStack &operator=(MutantStack const &other) { this->c = other.c; }
+    virtual ~MutantStack() {}
 
-  std::iterator<std::random_access_iterator_tag, std::stack<T>>
-    begin();
+    typedef typename std::deque<T>::iterator iterator;
 
-  std::iterator<std::random_access_iterator_tag, std::stack<T>>
-    end();
+    iterator begin() { return this->c.begin(); }
+    iterator end() { return this->c.end(); }
 };
 
 #endif // !MUTANTSTACK_HPP
--- nineth_list/ex02/main.cpp
+++ nineth_list/ex02/main.cpp
@@ -6,26 +6,68 @@
 /*   By: gvitor-s <gvitor-s@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/27 23:40:31 by gvitor-s          #+#    #+#             */
-/*   Updated: 2022/10/27 23:43:11 by gvitor-s         ###   ########.fr       */
+/*   Updated: 2022/10/29 00:47:18 by gvitor-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stack>
 #include <iostream>
+#include "MutantStack.hpp"
 
 int main ()
 {
-  std::stack<int> a;
+  {
+    MutantStack<int> mstack;
 
-  try {
-    a.push(1);
-    a.push(2);
-    a.pop();
-    a.pop();
-    a.pop();
+    mstack.push(5);
+    mstack.push(17);
+
+    std::cout << mstack.top() << '\n';
+    mstack.pop();
+
+    std::cout << mstack.size() << '\n';
+
+    mstack.push(3);
+    mstack.push(5);
+    mstack.push(737);
+    mstack.push(0);
+
+    MutantStack<int>::iterator it = mstack.begin();
+    MutantStack<int>::iterator ite = mstack.end();
+
+    ++it;
+    --it;
+    while (it != ite)
+    {
+      std::cout << *it << '\n';
+      ++it;
+    }
+
+    std::stack<int> s(mstack);
   }
-  catch (const std::exception&e) {
-    std::cerr << e.what() << '\n';
+  {
+    MutantStack<std::string> strStack;
+
+    strStack.push("Aragorn");
+    strStack.push("Gandalf");
+
+    std::cout << strStack.top() << '\n';
+    strStack.pop();
+    std::cout << strStack.top() << '\n';
+    strStack.pop();
+
+    strStack.push("I");
+    strStack.push("II");
+    strStack.push("III");
+
+    MutantStack<std::string>::iterator it = strStack.begin();
+    MutantStack<std::string>::iterator ite = strStack.end();
+
+    while (it != ite) {
+      std::cout << *it << ' ';
+      ++it;
+    }
+    std::cout << '\n';
   }
   return 0;
 }
